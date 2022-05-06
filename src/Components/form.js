0a319b9ea707539b47d09fada15bd0b5ca3ad714
mytrofanov <mytrofanovmaxym@gmail.com>
2022-05-06 06:55:44 +0300

    validation with regEx

--- src/Components/form.js
+++ src/Components/form.js
@@ -4,28 +4,50 @@ import styles from "./form.module.css";
 const Form = () => {
     const [name, setName] = useState('')
     const [number, setNumber] = useState('')
+    const [error, setError] = useState('')
+    const [errorDescription, setErrorDescription] = useState("This field in required")
+
+    const inputNameHandle = (text)=>{
+        if (/^[A-z ]+$/.test(text)){
+            setName(text)
+            setError('')
+            setErrorDescription('')
+       } else {
+            setError('inputName')
+            setErrorDescription('Only letters allowed')
+        }
+    }
 
 
     return (
         <div className={styles.formBlock}>
             <form action="">
+                <label htmlFor="Name"></label>
+                {error === 'inputName' && <div  className={styles.error}>
+                    Error</div> }
             <input
                 required
-                pattern="/^[a-zA-Z]+$/"
+                // pattern="/^[a-zA-Z]+$/"
                 type="text" id={'name'}
                 placeholder={'Name'}
-                className={styles.input}
+                className={error==='inputName' ? styles.badInputName : styles.inputName}
                 value={name}
-                onChange={e =>
-                    setName(e.target.value)}
+                onChange={e => {
+                    // setName(e.target.value)
+                    inputNameHandle(e.target.value)
+                }
+                    }
             />
+                {error === 'inputName' && <div  className={styles.errorMessage}>
+                    {errorDescription}</div> }
 
+                <label htmlFor="Phone"></label>
             <input
                 required
                 type="text"
-                id={'number'}
-                placeholder={'Number'}
-                className={styles.input}
+                id={'phone'}
+                placeholder={'Phone'}
+                className={styles.inputPhone}
                 value={number}
                 onChange={e =>
                     setNumber(e.target.value)}
